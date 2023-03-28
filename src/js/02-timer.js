9479d572f3a29698d3cc6294104f50cfc73edcab
Anton K <akarpenko83@gmail.com>
2023-03-28 17:29:27 +0300

    update task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,6 @@
-window.onload = function() {
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+window.onload = function () {
   Particles.init({
     selector: '.background',
     sizeVariations: 4,
@@ -30,13 +32,83 @@ window.onload = function() {
      
   });
 };
+
 const refs = {
-    inputRef: document.querySelector('input'),
-    btnRef: document.querySelector('button'),
+  inputRef: document.querySelector('input'),
+  btnRef: document.querySelector('button'),
+  daysRef: document.querySelector('span[data-days]'),
+  hoursRef: document.querySelector('span[data-hours]'),
+  minsRef: document.querySelector('span[data-minutes]'),
+  secRef: document.querySelector('span[data-seconds]'),
+}
+intervalId = null;
+refs.btnRef.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < Date.now())  {
+      alert('Please choose a date in the future');
+      return
+    } 
+    console.log(selectedDates[0]);
+    refs.btnRef.disabled = false;
+  },
+};
+
+const startDate = flatpickr("input#datetime-picker", options);
+
+const timer = {
+  start() {
+    const startTime = startDate.selectedDates[0];
+    const intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const ms = startTime - currentTime;
+      const { days, hours, minutes, seconds } = convertMs(ms);
+      refs.daysRef.textContent = days;
+      refs.hoursRef.textContent = hours;
+      refs.minsRef.textContent = minutes;
+      refs.secRef.textContent = seconds;
+      if (ms <= 0) {
+        clearInterval(intervalId);
+        resetInterface();
+      }
+
+    }, 1000);   
+  }
+};
+
+refs.btnRef.addEventListener('click', timer.start);
+
+function addLeadingZero(value) {
+return String(value).padStart(2, '0');
 }
 
-let timerId = null;
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-refs.inputRef.addEventListener('change', onChangeDate);
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-function onChangeDate() { };
+  return { days, hours, minutes, seconds };
+}
+
+function resetInterface() {
+      refs.daysRef.textContent = '00';
+      refs.hoursRef.textContent = '00';
+      refs.minsRef.textContent = '00';
+      refs.secRef.textContent = '00';
+ }
